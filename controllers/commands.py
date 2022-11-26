d14146a04b62fd4cda15d39b12483eb7565dc54a
João Vitor Freitas <65046228+Jwmffreitas@users.noreply.github.com>
2022-11-26 00:11:49 -0300

    Adding alerts to admins

--- controllers/commands.py
+++ controllers/commands.py
@@ -2,12 +2,20 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.update import Update
 from telegram.ext.callbackcontext import CallbackContext
 import os
+import datetime
+import pytz
 
 from services.aws import list_folders, get_hq, list_hqs
+from services.telegram import alert_admin
 
 def start(update: Update, context: CallbackContext):
     update.message.reply_text(
         "Hellooooo man, Bem-vindo ao bot. Manda o comando /help pra aprender a como usar :)")
+
+    username = update.message.from_user.username
+    name = update.message.from_user.full_name    
+    timezone = pytz.timezone("America/Sao_Paulo")
+    alert_admin(f'{name} (@{username}) chamou o Bot às {datetime.datetime.now().astimezone(timezone).strftime("%d/%m/%Y %H:%M")}')
 
 
 def help(update: Update, context: CallbackContext):
@@ -34,6 +42,10 @@ def list_publishers(update: Update, context: CallbackContext):
     update.message.reply_text("Escolha a editora para ver os títulos:", reply_markup=reply_markup)
 
 def list_editions(update: Update, context: CallbackContext):
+    username = update.message.from_user.username
+    name = update.message.from_user.full_name    
+    timezone = pytz.timezone("America/Sao_Paulo")
+
     if len(context.args) == 0:
         update.message.reply_text("""Para descobrir a quantidade de edições de uma HQ, é necessário passar a editora e o título.
         \n Exemplo: /edicoes MARVEL A_THOR""")
@@ -48,6 +60,7 @@ def list_editions(update: Update, context: CallbackContext):
     if not type(editions).__name__ == 'list':
         print(f'Não achei a lista de edições. Retornou: {editions}')
         update.message.reply_text(f'{editions}')
+        alert_admin(f'{name} (@{username}) Procurou por {context.args[0]}/{context.args[1]} às {datetime.datetime.now().astimezone(timezone).strftime("%d/%m/%Y %H:%M")} mas infelizmente não temos :(')
         return
     title = context.args[1].upper().replace('_', ' ')
     message = f'Nós temos {len(editions)} edições de {title}: \n'
@@ -61,6 +74,10 @@ def list_editions(update: Update, context: CallbackContext):
 
 
 def download(update: Update, context: CallbackContext):
+    username = update.message.from_user.username
+    name = update.message.from_user.full_name    
+    timezone = pytz.timezone("America/Sao_Paulo")
+
     if len(context.args) == 0:
         update.message.reply_text("""Para baixar uma HQ é necessário passar a editora, o título e a edição 
         \n Exemplo: /baixar MARVEL A_THOR 1""")
@@ -83,6 +100,7 @@ def download(update: Update, context: CallbackContext):
             update.message.reply_text('Poxa, ainda não temos esse título disponível para download :(')    
         else:
             update.message.reply_text('Sinto muito! Tivemos um erro ao buscar esse título :(')
+        alert_admin(f'{name} (@{username}) Procurou por {publisher}/{title}/{edition} às {datetime.datetime.now().astimezone(timezone).strftime("%d/%m/%Y %H:%M")} mas infelizmente não temos :(')
         return error 
     if os.path.exists(f'data/{title}_{edition}.pdf'):
         update.message.reply_text('Opa! Achei aqui. Só um segundo que já estou enviando...')
@@ -93,6 +111,7 @@ def download(update: Update, context: CallbackContext):
     print(f'HQ -> {new_title} Enviada!')
     print('-------------------')
     os.remove(hq_path)
+    alert_admin(f'{name} (@{username}) Solicitou {new_title} #{edition}.pdf às {datetime.datetime.now().astimezone(timezone).strftime("%d/%m/%Y %H:%M")}')
         
 
 def hqc_instagram(update: Update, context: CallbackContext):
---
+++ services/telegram.py
@@ -0,0 +1,12 @@
+import requests
+from settings import TELEGRAM_BOT_TOKEN, HQC_CHAT_ID
+
+def alert_admin(message):
+    url = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage?chat_id={HQC_CHAT_ID}&text={message}'
+    results = requests.get(url)
+    print('-------------------')
+    print(f'{message}')
+    print(f'Avisando os administradores do HQC...')
+    print('-------------------')
+
+#alert_admin('teste')
